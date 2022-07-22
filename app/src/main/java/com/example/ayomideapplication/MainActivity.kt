c85a1ec0c24170e203c378b5bef8f6f048696554
Fathia05 <93850624+Fathia05@users.noreply.github.com>
2022-07-22 00:11:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ayomideapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ayomideapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Log.i("Ayomide", "Tapped on Button")
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.teal_200))
+        }
+
+
+    }
+}
